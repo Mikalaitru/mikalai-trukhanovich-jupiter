97c0f3ba4c3206c82ee31a962c119cb45ce422ff
Mikalai <mikolka27667422@gmail.com>
2025-02-10 11:32:23 -0800

    Last lesson

--- JS/index.js
+++ JS/index.js
@@ -78,7 +78,8 @@ messageForm.addEventListener('submit', function (event) {
 
 const customProjectNames = {
     "mikalai-trukhanovich-jupiter": "Code the Dream Project: Portfolio",
-    "Code_The_Dream_Project_Open_API": "Code The Dream Project Open API"
+    "Code_The_Dream_Project_Open_API": "Code The Dream Project: Open API",
+    "intro-to-programming-debugging" : "Code The Dream Project: Debugging"
 };
 
 fetch('https://api.github.com/users/Mikalaitru/repos')
